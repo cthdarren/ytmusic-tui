a4016010e4346ca33aea7034271696f15aaa88ef
cthdarren <ctze.darren@gmail.com>
2024-05-03 15:40:13 +0800

    progress with list instead of textview for playlists

--- hello.go
+++ hello.go
@@ -1,28 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"strings"
-
+	// "github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	app := tview.NewApplication();
+	app := tview.NewApplication()
 
+	// Dummy data
+	// TODO: replace with API calls later on
 	playliststringtest := "wee duty mood,chill,poggers,kpop,jap weirdge songs,weeb,normie,rock,linkin park,nc25,bandori"
 	playlistArr := strings.Split(playliststringtest, ",")
 
-	var sb strings.Builder
-	for i, playlist := range playlistArr {
-		sb.WriteString(fmt.Sprintf("[\"%d\"]", i))
-		sb.WriteString(playlist)
-		sb.WriteString("[\"\"]\n")
-	}
+	// UI Elements
 	search := tview.NewTextView().SetText(" Search")
-	playlists := tview.NewTextView().SetRegions(true).SetText(sb.String())
-	nowplaying := tview.NewTextView().SetText("River Ed Sheeran")
+	playlists := tview.NewList()
 
+	nowplaying := tview.NewTextView().SetText("Fireworks Festival\nRadwimps")
 	grid := tview.NewGrid().
 		SetRows(1, 0, 3).
 		SetColumns(30, 0, 20).
@@ -30,16 +26,28 @@ func main() {
 		AddItem(search, 0, 0, 1, 3, 0, 10, false).
 		AddItem(nowplaying, 2, 0, 1, 3, 0, 0, false)
 
-		// Layout for screens narrower than 100 cells (menu and side bar are hidden).
+	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
 	grid.AddItem(playlists, 0, 0, 0, 0, 0, 0, true)
 
 	// Layout for screens wider than 100 cells.
 	// row index, col index, row span, col span, minheight, minwidth
 	grid.AddItem(playlists, 1, 0, 1, 1, 0, 100, true)
-	playlists.Highlight("1")
+
+	for _, playlist := range playlistArr {
+		playlists.AddItem(playlist, "", 0, func() { })
+	}
+
+	// playlists.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	// 	if event.Rune() == 'j' {
+	// 		return tcell.NewEventKey(tcell.KeyRune, 258, tcell.ModNone)
+	// 	}
+	// 	if event.Rune() == rune(tcell.KeyDown) {
+	// 		return nil
+	// 	}
+	// 	return event
+	// })
 
 	if err := app.SetRoot(grid, true).SetFocus(playlists).Run(); err != nil {
 		panic(err)
 	}
-
 }
