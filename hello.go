08c369497f3f445f99075e8799496eec307e5285
cthdarren <ctze.darren@gmail.com>
2024-05-03 09:23:17 +0800

    added tui package, playing around and getting used to go

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/rivo/tview"
+)
 
-func main(){
-	fmt.Println("Hello, World!")
+// User Interface
+func main() {
+
+	artistsBox := tview.NewTextView()
+	w := artistsBox.BatchWriter();
+	defer artistsBox.Clear();
+	fmt.Fprintln(w, "Nightcord at 25:00");
+	fmt.Fprintln(w, "Roselia");
+	fmt.Fprintln(w, "Linkin Park");
+	fmt.Fprintln(w, "IVE");
+	fmt.Fprintln(w, "Le Sserafim");
+	fmt.Println(artistsBox.GetText(false))
+
+	//box := tview.NewBox().SetBorder(true).SetTitle("YTM TUI!");
+
+	if err := tview.NewApplication().SetRoot(artistsBox, true).Run(); err != nil {
+		panic(err)
+	}
 }
